746bcab3c6e52587960e1b1bb3cff16f095654f7
mc <mc@mcdeMacBook-Pro.local>
2018-06-12 14:32:02 +0800

    修复看书中源的解析问题

--- src/parser/kanshuz.parser.js
+++ src/parser/kanshuz.parser.js
@@ -19,18 +19,4 @@ KanshuzParser.prototype = new CommonParser();
 
 // XslaParser.prototype.getChapterList = function(url){ console.log(url); }
 
-KanshuzParser.prototype.getChapterDetail = async function (urlx) {
-  let res = await this.getPageContent.call(this, urlx);
-  if (res === '-1') { return ''; }
-  const $ = cheerio.load(res, { decodeEntities: false });
-  let asTit = $(this.chapterDetail.titleSelector);
-  let asCon = $(this.chapterDetail.contentSelector);
-  asCon = asCon[0].children[2].data;
-  let arr = {
-    title: asTit[0].children[0].data.split('_')[0],
-    content: asCon
-  };
-  return arr;
-}
-
 module.exports = KanshuzParser;
