cb4ecf824c40a4752beaa29f1751a0c1f71368a8
czq <chenzhengqiang@youzan.com>
2018-12-18 21:04:00 +0800

    fix: 规范返回值

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,10 +9,11 @@ const analysis = require('./analysis');
 const router = Router();
 
 router.use('/', home.routes(), home.allowedMethods());
-router.use('/sear', search.routes(), search.allowedMethods());
 router.use('/apidoc', apidoc.routes(), apidoc.allowedMethods());
-router.use('/rnklist', rnklist.routes(), rnklist.allowedMethods());
-router.use('/analysis', analysis.routes(), analysis.allowedMethods());
+
+router.use('/v2/sear', search.routes(), search.allowedMethods());
+router.use('/v2/rnklist', rnklist.routes(), rnklist.allowedMethods());
+router.use('/v2/analysis', analysis.routes(), analysis.allowedMethods());
 
 router.get('*', async (ctx) => {
   ctx.body = 'Request Error';
