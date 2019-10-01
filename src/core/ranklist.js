a1d908957589ba48deac3cdb88b57946653fe277
czq <chenzhengqiang@youzan.com>
2019-10-01 15:00:58 +0800

    fix: 修复一些问题

--- src/core/ranklist.js
+++ src/core/ranklist.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
+const _get = require('lodash/get');
 
 const { crawlPage } = require('../util/http-req');
 
@@ -11,7 +12,7 @@ async function qdMMRnkLst(x) {
 }
 
 async function qdGGRnkList(x) {
-  const url = `http://r.qidian.com/yuepiao?style=2&page=${x}`;
+  const url = `https://www.qidian.com/rank/yuepiao?style=2&page=${x}`;
   const res = await crawlPage(url);
   return qdParser(res);
 }
@@ -21,16 +22,17 @@ function qdParser(res) {
   res = iconv.decode(res, 'UTF-8');
   const $ = cheerio.load(res, { decodeEntities: false });
   const ass = $('.book-text tbody tr');
+
   let $2, asn;
   for (let i = 0, size = ass.length; i < size; i++) {
     $2 = cheerio.load(ass[i], { decodeEntities: false });
     asn = $2('td');
     if (asn.length < 2) continue;
     rankList.push({
-      type: asn[1].children[0].children[1].data,
-      name: asn[2].children[0].children[0].data,
-      latestChapter: asn[3].children[0].children[0].data,
-      author: asn[5].children[0].children[0].data
+      type: _get(asn, '[1].children[0].children[1].data', '-'),
+      name: _get(asn, '[2].children[0].children[0].data', '-'),
+      latestChapter: _get(asn, '[3].children[0].children[0].data', '-'),
+      author: _get(asn, '[5].children[0].children[0].data', '-')
     });
   }
   return rankList;
